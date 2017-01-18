b1154ec790c83194d04769c1439b6ac1e5d73486
Philipp Faller <philipp.faller@yahoo.de>
2017-01-18 15:11:57 +0100

    Test für Data

---
+++ implementierung/ellipse/test/data/AchievementTest.java
@@ -0,0 +1,24 @@
+package data;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AchievementTest extends DataTest {
+
+    @Test
+    public void testID() {
+        int id = 42;
+        Achievement a = new Achievement();
+        a.setId(id);
+        assertEquals(id, a.getId());
+    }
+
+    @Test
+    public void testName() {
+        String name = "Name";
+        Achievement a = new Achievement();
+        a.setName(name);
+        assertEquals(name, a.getName());
+    }
+}
