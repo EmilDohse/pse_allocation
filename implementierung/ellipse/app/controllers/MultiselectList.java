e5910633200994d25340b2a4eeaee1478848af31
Daniel Biester <danielbiester@icloud.com>
2017-02-06 14:43:50 +0100

    Multiselect auslesen verbessert und TestCode aus IndexController gelöscht.

---
+++ implementierung/ellipse/app/controllers/MultiselectList.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import play.data.DynamicForm;
+
+/**
+ * Hilfsklasse um Multiselect-Listen auszulesen, da diese nicht standardisiert
+ * sind und Play nicht richtig mit ihnen umgehen kann.
+ * 
+ * @author daniel
+ *
+ */
+public class MultiselectList {
+
+    /**
+     * Diese Mehode gibt alle Multiselect-Values in Array-Form zurück. Als Name
+     * muss der name-Tag des select-Elementes mitgegeben werden, jedoch ohne die
+     * "[]" am Ende eines multiselect-Elementes.
+     * 
+     * @param form
+     *            das Formular das die Daten enthält.
+     * @param name
+     *            der Name des select-Elementes.
+     * @return ein Array mit Strings an selektierten Werten.
+     */
+    public static String[] getValueArray(DynamicForm form, String name) {
+        Map<String, String> data = form.data();
+        ArrayList<String> result = new ArrayList<>();
+        for (Entry<String, String> entry : data.entrySet()) {
+            if (entry.getKey().contains(name)) {
+                result.add(entry.getValue());
+            }
+        }
+        String[] array = new String[result.size()];
+        return result.toArray(array);
+    }
+}
