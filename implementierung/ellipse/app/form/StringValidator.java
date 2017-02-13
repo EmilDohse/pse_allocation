2c64ed6f4528a13d6c6de3e77b3dbac83a95efd0
Lars Seufert <uuede@student.kit.edu>
2017-02-13 03:18:11 +0100

    Javadoc für package form

--- implementierung/ellipse/app/form/StringValidator.java
+++ implementierung/ellipse/app/form/StringValidator.java
@@ -1,5 +1,9 @@
 package form;
 
+
+/**
+ * Klasse um einen String zu validieren.
+ */
 public class StringValidator implements Validator<String> {
 
     private int    min;
@@ -8,20 +12,50 @@ public class StringValidator implements Validator<String> {
 
     private String msg = "INTERNAL_ERROR";
 
+    /**
+     * Konstruktor mit minimaler Stringlänge.
+     * 
+     * @param min
+     *            minimale Länge des Strings.
+     */
     public StringValidator(int min) {
         this(min, Integer.MAX_VALUE);
     }
 
+    /**
+     * Konstruktor mit minimaler und maximaler Stringlänge.
+     * 
+     * @param min
+     *            minimale Länge des Strings.
+     * @param max
+     *            maximale Länge des Strings.
+     */
     public StringValidator(int min, int max) {
         this(min, max, null);
     }
 
+    /**
+     * Konstruktor mit minimaler und maximaler Stringlänge und Regex.
+     * 
+     * @param min
+     *            minimale Länge des Strings.
+     * @param max
+     *            maximale Länge des Strings.
+     * @param regex
+     *            Regex, zu der der String passen muss.
+     */
     public StringValidator(int min, int max, String regex) {
         this.min = min;
         this.max = max;
         this.regex = regex;
     }
 
+    /**
+     * Setzt die Errormessage.
+     * 
+     * @param message
+     *            Errormessage.
+     */
     public void setMessage(String message) {
         this.msg = message;
     }
