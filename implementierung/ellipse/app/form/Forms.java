601bbf1ccfc68b81e99990fca4b786ec3cc11e4c
Daniel Biester <danielbiester@icloud.com>
2017-02-13 02:53:48 +0100

    Deletion of unused code.

--- implementierung/ellipse/app/form/Forms.java
+++ implementierung/ellipse/app/form/Forms.java
@@ -2,62 +2,6 @@ package form;
 
 public class Forms {
 
-    public class Admin {
-
-        public class EditAccount {
-
-            public static final String OLD_PASSWORD    = "oldPassword";
-            public static final String NEW_PASSWORD    = "newPassowrd";
-            public static final String NEW_PASSWORD_RP = "newPasswordRepeat";
-
-        }
-
-        public class CreateAdviser {
-
-            public static final String FIRST_NAME = "firstName";
-            public static final String LAST_NAME  = "lastName";
-            public static final String EMAIL      = "email";
-            public static final String PASSWORD   = "password";
-
-        }
-
-        public class AddAllocation {
-
-            public static final String NAME                = "name";
-            public static final String MIN_TEAM_SIZE       = "minTeamSize";
-            public static final String MAX_TEAM_SIZE       = "maxTeamSize";
-            public static final String PREFFERED_TEAM_SIZE = "prefferedTeamSize";
-
-        }
-
-        public class Import {
-
-            public static final String FILE       = "file";
-            public static final String ALLOCATION = "allocation";
-            public static final String SPO        = "spo";
-            public static final String STUDENTS   = "students";
-            public static final String PROJECTS   = "projects";
-
-        }
-
-        public class AddProject {
-
-            public static final String NAME = "name";
-        }
-
-        public class ChangeSPO {
-
-            public static final String ID   = "id";
-            public static final String NAME = "nameSPO";
-        }
-
-        public class AddAchievement {
-
-            public static final String ID   = "id";
-            public static final String NAME = "nameAchiev";
-        }
-    }
-
     public static StringValidator getPasswordValidator() {
         StringValidator validator = new StringValidator(6);
         validator.setMessage("general.error.minimalPasswordLegth");
@@ -65,7 +9,8 @@ public class Forms {
     }
 
     public static StringValidator getEmailValidator() {
-        StringValidator validator = new StringValidator(1, Integer.MAX_VALUE, ".+@.+");
+        StringValidator validator = new StringValidator(1, Integer.MAX_VALUE,
+                ".+@.+");
         validator.setMessage("user.noValidEmail");
         return validator;
     }
