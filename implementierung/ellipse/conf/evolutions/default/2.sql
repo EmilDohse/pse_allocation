d203dd1825041d8bbfea075b17e8b0998518860d
Philipp Loth <philipp.loth@arcor.de>
2017-02-13 01:11:18 +0100

    Evolution gefixt

--- implementierung/ellipse/conf/evolutions/default/2.sql
+++ implementierung/ellipse/conf/evolutions/default/2.sql
@@ -3,7 +3,7 @@ insert into administrator (id, username, password, email_address, first_name, la
 values (1, 'admin', '$2a$10$nVJZIOx02y8lk5L3N4MAhuE1QFjBKKQDEDBvTHFCc7Jmw4jQPbhG.', 'admin@kit.edu', 'admin', 'admin');
 
 insert into semester (id, wintersemester, max_group_size, year, name, info_text, final_allocation_id, registration_start, registration_end)
-values (1, 0, 6, 2017, 'default', 'Default Semester', 0, 1486636365000, 1486636365000);
+values (1, 0, 6, 2017, 'default', 'Default Semester', NULL, 1486636365000, 1486636365000);
 
 insert into general_data (id, current_semester_id)
 values (1, 1);
