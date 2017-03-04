9d04f3e2bab8a76da6410355452e1a5c8cbd6ece
Daniel Biester <danielbiester@icloud.com>
2017-03-05 00:29:06 +0100

    ImportControllerTests geschrieben, fürn export kommen die morgen noch. BadRequest auch angefangen, hängt aber an der Authentication...

---
+++ implementierung/ellipse/test/BadRequestTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+
+import play.mvc.Http.RequestBuilder;
+import play.libs.ws.WS;
+import play.libs.ws.WSClient;
+import play.libs.ws.WSResponse;
+import play.mvc.Result;
+import play.test.WithServer;
+import views.TestHelpers;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.CompletionStage;
+
+public class BadRequestTest extends WithServer {
+
+    @Test
+    public void indexCreateBadReq() throws Exception {
+        TestHelpers.setStateToRegistration();
+        testBadRequest("create");
+    }
+
+    @Test
+    public void indexRequestResetBadReq() throws Exception {
+        testBadRequest("requestReset");
+    }
+
+    private void testBadRequest(String path) throws Exception {
+        int timeout = 5000;
+        String url = "http://localhost:" + this.testServer.port() + "/" + path;
+        try (WSClient ws = WS.newClient(this.testServer.port())) {
+            CompletionStage<WSResponse> stage = ws.url(url).post((String) null);
+            WSResponse response = stage.toCompletableFuture().get();
+            assertEquals(400, response.getStatus());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- implementierung/ellipse/test/views/pages/admin/AdminImExportPage.java
+++ implementierung/ellipse/test/views/pages/admin/AdminImExportPage.java
@@ -1,5 +1,6 @@
 package views.pages.admin;
 
+import play.test.TestBrowser;
 import views.pages.Page;
 
 public class AdminImExportPage extends Page {
@@ -7,5 +8,29 @@ public class AdminImExportPage extends Page {
     @Override
     public String getUrl() {
         return "/admin/exportImport";
+    }
+
+    public boolean showsError(TestBrowser browser) {
+        return !browser.$(".alert-danger").isEmpty();
+    }
+
+    public void importSpo(TestBrowser browser, String filePath) {
+        browser.$("#file").first().fill().with(filePath);
+        browser.$("#submit_spo_import").first().click();
+    }
+
+    public void importAllocation(TestBrowser browser, String filePath) {
+        browser.$("#file").first().fill().with(filePath);
+        browser.$("#submit_allocation_import").first().click();
+    }
+
+    public void importStudents(TestBrowser browser, String filePath) {
+        browser.$("#file").first().fill().with(filePath);
+        browser.$("#submit_students_import").first().click();
+    }
+
+    public void importProjects(TestBrowser browser, String filePath) {
+        browser.$("#file").first().fill().with(filePath);
+        browser.$("#submit_projects_import").first().click();
     }
 }
