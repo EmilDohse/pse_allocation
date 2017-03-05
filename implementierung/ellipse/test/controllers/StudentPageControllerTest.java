857bda0ba191b8364d5754005f54e80f018f7507
Philipp Faller <philipp.faller@yahoo.de>
2017-03-05 22:29:36 +0100

    Tests zu STudentPageCOntroller hinzugefügt

--- implementierung/ellipse/test/controllers/StudentPageControllerTest.java
+++ implementierung/ellipse/test/controllers/StudentPageControllerTest.java
@@ -2,6 +2,7 @@ package controllers;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
@@ -23,7 +24,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import play.mvc.Http.Context;
 import data.Achievement;
 import data.GeneralData;
 import data.LearningGroup;
@@ -32,6 +32,7 @@ import data.SPO;
 import data.Semester;
 import data.Student;
 import notificationSystem.Notifier;
+import play.mvc.Http.Context;
 import security.BlowfishPasswordEncoder;
 import security.UserManagement;
 
@@ -569,6 +570,55 @@ public class StudentPageControllerTest extends ControllerTest {
         assertFalse(semester.getLearningGroups().contains(learningGroup));
         assertEquals(name, lg.getName());
         assertTrue(enc.matches(password, lg.getPassword()));
+
+    }
+
+    @Test
+    public void leaveLearningGroupTest() {
+        when(userManagement.getUserProfile(any())).thenReturn(student);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1");
+        when(form.data()).thenReturn(map);
+        learningGroup.doTransaction(() -> learningGroup.setPrivate(false));
+
+        controller.leaveLearningGroup();
+
+        semester.refresh();
+        LearningGroup lg = semester.getLearningGroupOf(student);
+        lg.refresh();
+
+        assertFalse(lg.equals(learningGroup));
+        assertTrue(lg.isPrivate());
+        assertEquals(student.getUserName(), lg.getName());
+        learningGroup.getRatings().forEach(r -> {
+            int newRating = lg.getRating(r.getProject());
+            assertEquals(r.getRating(), newRating);
+        });
+        assertNull(LearningGroup.getById(LearningGroup.class,
+                learningGroup.getId()));
+    }
+
+    @Test
+    public void leavePrivateLearningGroupTest() {
+        when(userManagement.getUserProfile(any())).thenReturn(student);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1");
+        when(form.data()).thenReturn(map);
+
+        controller.leaveLearningGroup();
+
+        semester.refresh();
+        LearningGroup lg = semester.getLearningGroupOf(student);
+        lg.refresh();
+
+        assertEquals(lg, learningGroup);
+        assertTrue(lg.isPrivate());
+        learningGroup.getRatings().forEach(r -> {
+            int newRating = lg.getRating(r.getProject());
+            assertEquals(r.getRating(), newRating);
+        });
 
     }
 
