71acae0f6bb84d82984f0f10909e17c8b177d351
Philipp Loth <philipp.loth@arcor.de>
2017-03-05 22:29:34 +0100

    Tests gefixt

--- implementierung/ellipse/app/controllers/AdminProjectController.java
+++ implementierung/ellipse/app/controllers/AdminProjectController.java
@@ -55,7 +55,8 @@ public class AdminProjectController extends Controller {
             semester.addProject(project);
         });
         int projID = project.getId();
-        return redirect(controllers.routes.AdminPageController.projectEditPage(projID));
+        return redirect(
+                controllers.routes.AdminPageController.projectEditPage(projID));
 
     }
 
@@ -72,10 +73,14 @@ public class AdminProjectController extends Controller {
             if (form.data().isEmpty()) {
                 return badRequest(ctx().messages().at(INTERNAL_ERROR));
             }
-            Project project = ElipseModel.getById(Project.class, Integer.parseInt(form.get("id")));
+            Project project = ElipseModel.getById(Project.class,
+                    Integer.parseInt(form.get("id")));
+            project.doTransaction(() -> {
+                project.setAdvisers(new ArrayList<>());
+            });
             project.delete();
-
-            return redirect(controllers.routes.AdminPageController.projectPage());
+            return redirect(
+                    controllers.routes.AdminPageController.projectPage());
         }
     }
 
@@ -95,7 +100,8 @@ public class AdminProjectController extends Controller {
                 if (form.data().isEmpty()) {
                     return badRequest(ctx().messages().at(INTERNAL_ERROR));
                 }
-                StringValidator notEmptyValidator = Forms.getNonEmptyStringValidator();
+                StringValidator notEmptyValidator = Forms
+                        .getNonEmptyStringValidator();
                 IntValidator minValidator = new IntValidator(0);
 
                 String projName;
@@ -111,54 +117,67 @@ public class AdminProjectController extends Controller {
                     id = minValidator.validate(form.get("id"));
                 } catch (ValidationException e) {
                     flash(ERROR, ctx().messages().at(e.getMessage()));
-                    return redirect(controllers.routes.AdminPageController.projectEditPage(-1));
+                    return redirect(controllers.routes.AdminPageController
+                            .projectEditPage(-1));
                 }
                 Project project = ElipseModel.getById(Project.class, id);
                 // fehlerausgabe wenn projekt gelöscht wurde während der
                 // bearbeitung
                 if (project == null) {
-                    flash(ERROR, ctx().messages().at(Project.CONCURRENCY_ERROR));
-                    return redirect(controllers.routes.AdminPageController.projectPage());
+                    flash(ERROR,
+                            ctx().messages().at(Project.CONCURRENCY_ERROR));
+                    return redirect(controllers.routes.AdminPageController
+                            .projectPage());
                 }
                 // Prüfe alle ausgelesenen Werte auf nichtleere Strings und
                 // Mindestwerte
                 try {
                     projName = notEmptyValidator.validate(form.get("name"));
                     url = notEmptyValidator.validate(form.get("url"));
-                    institute = notEmptyValidator.validate(form.get("institute"));
-                    description = notEmptyValidator.validate(form.get("description"));
-                    numberOfTeams = minValidator.validate(form.get("teamCount"));
+                    institute = notEmptyValidator
+                            .validate(form.get("institute"));
+                    description = notEmptyValidator
+                            .validate(form.get("description"));
+                    numberOfTeams = minValidator
+                            .validate(form.get("teamCount"));
                     minSize = minValidator.validate(form.get("minSize"));
                     maxSize = minValidator.validate(form.get("maxSize"));
                 } catch (ValidationException e) {
                     flash(ERROR, ctx().messages().at("error.wrongInput"));
-                    return redirect(controllers.routes.AdminPageController.projectEditPage(project.getId()));
+                    return redirect(controllers.routes.AdminPageController
+                            .projectEditPage(project.getId()));
                 }
                 // Prüfe, dass max >= min und dass Min und Max immer
                 // gleichzeitig 0 oder
                 // nichtnull sind
                 if ((minSize == 0 ^ maxSize == 0) || (maxSize < minSize)) {
                     flash(ERROR, ctx().messages().at("error.wrongInput"));
-                    return redirect(controllers.routes.AdminPageController.projectEditPage(project.getId()));
+                    return redirect(controllers.routes.AdminPageController
+                            .projectEditPage(project.getId()));
                 }
 
                 // Hole die Betreuer des Projektes aus der Datenbank
                 ArrayList<Adviser> advisers = new ArrayList<>();
-                String[] adviserIds = MultiselectList.getValueArray(form, "adviser-multiselect");
+                String[] adviserIds = MultiselectList.getValueArray(form,
+                        "adviser-multiselect");
                 for (String adviserIdString : adviserIds) {
                     int adviserId;
                     try {
                         adviserId = Integer.parseInt(adviserIdString);
                     } catch (NumberFormatException e) {
                         flash(ERROR, ctx().messages().at(INTERNAL_ERROR));
-                        return redirect(controllers.routes.AdminPageController.projectEditPage(project.getId()));
+                        return redirect(controllers.routes.AdminPageController
+                                .projectEditPage(project.getId()));
                     }
                     // hier wird überprüft ob der adviser während der
                     // bearbeitung gelöscht wurde
-                    Adviser adviser = ElipseModel.getById(Adviser.class, adviserId);
+                    Adviser adviser = ElipseModel.getById(Adviser.class,
+                            adviserId);
                     if (adviser == null) {
-                        flash(ERROR, ctx().messages().at(Adviser.CONCURRENCY_ERROR));
-                        return redirect(controllers.routes.AdminPageController.projectEditPage(project.getId()));
+                        flash(ERROR,
+                                ctx().messages().at(Adviser.CONCURRENCY_ERROR));
+                        return redirect(controllers.routes.AdminPageController
+                                .projectEditPage(project.getId()));
                     }
                     advisers.add(adviser);
                 }
@@ -175,7 +194,8 @@ public class AdminProjectController extends Controller {
                     project.setProjectURL(url);
                 });
 
-                return redirect(controllers.routes.AdminPageController.projectPage());
+                return redirect(
+                        controllers.routes.AdminPageController.projectPage());
             }
         }
     }
