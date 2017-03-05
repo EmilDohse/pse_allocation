7a60cedb0eb3ff4e6802cebb8a7512f6d4628e32
Sam <samuel@kopmann.com>
2017-03-05 16:39:46 +0100

    Test Index Student pages doc

--- implementierung/ellipse/test/views/pages/index/IndexPage.java
+++ implementierung/ellipse/test/views/pages/index/IndexPage.java
@@ -7,12 +7,26 @@ import org.fluentlenium.core.FluentPage;
 import play.test.TestBrowser;
 import views.pages.Page;
 
+/**
+ * Diese Klasse beinhaltet Methoden für den Inhalt der Indexseite.
+ */
 public class IndexPage extends Page {
 
+    /**
+     * Diese Methode vollzieht den Wechsel zu Passwort.Zurücksetzen Seite.
+     * 
+     * @param browser
+     *            Der TestBrowser.
+     */
     public void gotoPasswordResetPage(TestBrowser browser) {
         browser.$("#pwReset").click();
     }
 
+    /**
+     * Diese Methode gibt die Url der Seite zurück.
+     * 
+     * @return Die Url der Seite.
+     */
     @Override
     public String getUrl() {
         return "/";
